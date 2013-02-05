2e8277894a18ca92315bf935ba29fa4b9dbd027b
Jason Green <jason@Green.cx>
2013-02-05 12:58:21 -0600

    test com blog

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -10,8 +10,8 @@ TUMBLR_API_URL = 'http://api.tumblr.com/v2/blog/{0}'.format(TUMBLR_BLOG_URL)
 TUMBLR_API_KEY = '[ENTER TUMBLR API KEY HERE, SEE TUMBLR SETUP INSTRUCTIONS]'
 
 #Blog Integration: Wordpress
-WORDPRESS_BLOG_URL = 'http://poteaupressdotcom.wordpress.com'
-WORDPRESS_API_URL = 'https://public-api.wordpress.com/rest/v1/sites/poteaupressdotcom.wordpress.com'
+WORDPRESS_BLOG_URL = 'greenthe.me'
+WORDPRESS_API_URL = 'https://public-api.wordpress.com/rest/v1/sites/grenthe.me'
 #.format(WORDPRESS_BLOG_URL)
 
 #RSS Feed Integration: (by default use Tumblr rss feed)
